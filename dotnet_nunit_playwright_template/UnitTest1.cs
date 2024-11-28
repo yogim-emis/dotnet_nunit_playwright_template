4fb18ddcd985c5730da3a94596c96f532e3c632d
Yogesh Mistry <yogesh.mistry@emishealth.com>
2024-11-28 15:56:12 +0000

    Cleared down dot/c#/nunit/playwright repo

---
+++ dotnet_nunit_playwright_template/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace dotnet_nunit_playwright_template;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
